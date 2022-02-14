5f54aaf53b7bb36ecc29ed22ec271a60cd73ea0f
Robert Jones <roberthuwjones@outlook.com>
2022-02-14 17:35:32 +0000

    text started

---
+++ story.py
@@ -0,0 +1,4 @@
+# take in the player's name
+username = input('You look up from the empty glass to see the bartender scowling at you. "What did you say your name was again?" he growls \n')
+print('"' + '...' + username + '"' + " you mutter bitterly \n")
+answer = input('"Oh yeah? Well ' + username + ' my friend" (the sarcasm in his voice is palpable), "I was just wondering if you have the cash to actually pay for all those drinks? Do you?"')
